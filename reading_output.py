73935180218d1445640c0840f2e349559a071a26
kulendu <imkulendu@gmail.com>
2023-12-20 22:47:49 +0530

    adding parameters from GPU --> CPU
    also adding the file to read the .pkl file

---
+++ reading_output.py
@@ -0,0 +1,7 @@
+import pickle
+
+a = open("./output/demo_video.mov/vibe_output.pkl", 'r', encoding="latin1")
+b = pickle.load(a)
+
+
+print(data)
--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir -p data
+# download the .zip file from this: https://drive.google.com/uc?id=1untXhYOLQtpNEy4GTY_0fL_H-k6cTf_r link, and run this file in ''./''
+
+#mkdir -p data
 cd data
-gdown "https://drive.google.com/uc?id=1untXhYOLQtpNEy4GTY_0fL_H-k6cTf_r"
+#gdown "https://drive.google.com/file/d/1qfJjZrFPakw4DN3B_LLI7-R7xDF1FIkY/view?usp=sharing"
 unzip vibe_data.zip
 rm vibe_data.zip
 cd ..
